543dc5367d6c5980f3c5584cb0862d0128a51e4d
JFeoOks <jfeoks-java@yandex.ru>
2020-02-28 01:45:25 +0300

    todo app basics

--- App.js
+++ App.js
@@ -1,19 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View, ScrollView } from "react-native";
+import { Navbar } from "./src/Navbar";
+import { AddTodo } from "./src/AddTodo";
+import { Todo } from "./src/Todo";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, title: 'test'},
+    {id: 2, title: 'test'},
+    {id: 3, title: 'test'},
+    {id: 4, title: 'test'},
+    {id: 5, title: 'test'},
+    {id: 6, title: 'test'},
+    {id: 7, title: 'test'},
+    {id: 8, title: 'test'}
+  ]);
+
+  const addTodo = title => {
+    // const newTodo = {
+    //   id: Date.now().toString(),
+    //   title: title
+    // }
+
+    // setTodos(todos.concat([newTodo]))
+    // setTodos((prevTodos) => {
+    //   return [
+    //     ...prevTodos,
+    //     newTodo
+    //   ]
+    // })
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Navbar title="Todo App!" />
+      <ScrollView style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+
+        <View>
+          {todos.map(todo => (
+            <Todo todo={todo} key={todo.id} />
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingHorizontal: 30,
+    paddingVertical: 20
+  }
 });
